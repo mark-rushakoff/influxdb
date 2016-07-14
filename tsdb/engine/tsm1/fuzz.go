4657cd7d940871545e8792e948575b769a98811f
Mark Rushakoff <mark@influxdb.com>
2016-07-13 18:35:28 -0600

    Add fuzzing for tsm1.CacheLoader

---
+++ tsdb/engine/tsm1/fuzz.go
@@ -0,0 +1,66 @@
+// +build gofuzz
+package tsm1
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/influxdata/influxdb/tsdb"
+)
+
+// Fuzzing definitions to detect edge/corner cases via https://github.com/dvyukov/go-fuzz.
+//
+// Currently, go-fuzz doesn't offer a way to automatically detect one of multiple Fuzz functions,
+// so you will need to specify which function to use.
+// First, build the instrumented package:
+//
+//     go-fuzz-build -func FuzzCacheLoader -o /tmp/FuzzCacheLoader.zip github.com/influxdata/influxdb/tsdb/engine/tsm1
+//
+// Then, run the tests:
+//
+//     go-fuzz -bin=/tmp/FuzzCacheLoader.zip -workdir=$GOPATH/src/github.com/influxdata/influxdb/tsdb/engine/tsm1/fuzz-data/CacheLoader
+
+// The return value of the fuzz functions affects how the fuzzer finds new cases.
+const (
+	// "Correct" input that was correctly parsed.
+	fuzzInteresting = 1
+
+	// Not really interesting input.
+	fuzzBoring = 0
+
+	// Do not use this input, even if it results in new coverage.
+	// (When would you use this?)
+	fuzzIgnore = -1
+)
+
+func FuzzCacheLoader(data []byte) int {
+	// Write the data to a file since CacheLoader expects to load a collection of files.
+	f, err := ioutil.TempFile("", "cacheloader-fuzz")
+	if err != nil {
+		// Should never happen. Has nothing to do with input data.
+		return fuzzBoring
+	}
+	defer os.Remove(f.Name())
+
+	if _, err := f.Write(data); err != nil {
+		// Should never happen. Has nothing to do with input data.
+		return fuzzBoring
+	}
+	if err := f.Close(); err != nil {
+		// Should never happen. Has nothing to do with input data.
+		return fuzzBoring
+	}
+
+	cl := NewCacheLoader([]string{f.Name()})
+	cl.Logger.SetOutput(ioutil.Discard)
+
+	// CacheLoader populates a cache struct.
+	// TODO: remove path argument?
+	c := NewCache(tsdb.DefaultCacheMaxMemorySize, "")
+
+	if err := cl.Load(c); err != nil {
+		return fuzzBoring
+	}
+
+	return fuzzInteresting
+}
